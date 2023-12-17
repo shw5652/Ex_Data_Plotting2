2bbc73738e831fc50b71ca2d766fb62bff2540ba
Shweta Kumari <103035818+shw5652@users.noreply.github.com>
2023-12-17 15:16:49 +0530

    Add files via upload

---
+++ plot5.R
@@ -0,0 +1,27 @@
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+SCC <- readRDS(file = "Source_Classification_Code.rds")
+NEI <- readRDS(file = "summarySCC_PM25.rds")
+
+vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- SCC[vehicles,]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
+
+# Subset the vehicles NEI data to Baltimore's fip
+baltimoreVehiclesNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
+
+png("plot5.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
+
+print(ggp)
+
+dev.off()
---
+++ plot6.R
@@ -0,0 +1,35 @@
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+SCC <- readRDS(file = "Source_Classification_Code.rds")
+NEI <- readRDS(file = "summarySCC_PM25.rds")
+
+vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- SCC[vehicles,]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
+
+# Subset the vehicles NEI data by each city's fip and add city name.
+vehiclesBaltimoreNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
+vehiclesBaltimoreNEI$city <- "Baltimore City"
+
+vehiclesLANEI <- vehiclesNEI[vehiclesNEI$fips=="06037",]
+vehiclesLANEI$city <- "Los Angeles County"
+
+# Combine the two subsets with city name into one data frame
+bothNEI <- rbind(vehiclesBaltimoreNEI,vehiclesLANEI)
+
+png("plot6.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(bothNEI, aes(x=factor(year), y=Emissions, fill=city)) +
+  geom_bar(aes(fill=year),stat="identity") +
+  facet_grid(scales="free", space="free", .~city) +
+  guides(fill=FALSE) + theme_bw() +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+
+print(ggp)
+
+dev.off()
